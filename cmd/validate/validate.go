f1a43d504a5945f4cd8d2ba49c242f55ec349aa7
keith <keithcompere150@gmail.com>
2025-01-26 23:48:56 -0500

    yay cli

---
+++ cmd/validate/validate.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2025 Keith Compere <KeithCompere150@gmail.com>
+*/
+package validate
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+)
+
+var (
+	dbURL string
+)
+
+// validateCmd represents the validate command
+var ValidateCmd = &cobra.Command{
+	Use:   "validate",
+	Short: "The palette responsible for schema validation",
+	Long: `This pallete is responsible for ensuring that the database schema has no
+		cycles. If a cycle is detected, a series of recommendation queries will be generated`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+	ValidateCmd.AddCommand(schemaCmd)
+	schemaCmd.PersistentFlags().StringVarP(&dbURL, "database", "d", "", "url to connect to the database with")
+
+	if err := schemaCmd.MarkPersistentFlagRequired("database"); err != nil {
+		log.Fatal(err)
+	}
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// validateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// validateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
